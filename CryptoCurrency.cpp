e0851e6418eeab06610ca48b680324eeada3d26a
DazaiDostrovesky <nolongersane123@outlook.com>
2025-07-18 15:27:13 +0000

    Updated CryptoCurrency.cpp

--- CryptoCurrency.cpp
+++ CryptoCurrency.cpp
@@ -1,41 +1,56 @@
-#include <iostream>
-#include <string>
+nclude <iostream>
+#include <curl/curl.h>
+#include "json.hpp"
 using namespace std;
-string website_api(string coin_address, int chainid) {
-    
-    string url;
-    switch (chainid) {
-    	case 1: {
-    		url = "https://api.etherscan.io/api?module=account&action=balance&address=" + coin_address + "&tag=latest";
-    		
-			break;
-		}
-		case 2:{
-			url =  url = "https://api.etherscan.io/api?module=account&action=balance&address=" + coin_address + "&tag=latest";
-			break;
-		}
-		default: {
-			  return "Unsupported chain ID";
-		}
-	}
+using json = nlohmann::json;
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, string* data) {
+    data->append((char*)contents, size * nmemb);
+    return size * nmemb;
 }
 
-int main()
-{	
-    string	address_coin;
-	cout<<"Please enter coin address: "<<endl;
-	cin>>address_coin;
-	
-	int id;
-	cout<<"Please enter chain id: "<<endl;
-	cin>>id;
-	
-	string api_response = website_api(address_coin, id);
-    cout << "API Response: " << api_response << endl;
+int main() {
+    string input;
+    cout << "Enter coin name or contract address: ";
+    cin >> input;  // Simple input without getline
 
-	return 0;
+    CURL* curl = curl_easy_init();
+    if (!curl) {
+        cerr << "Failed to initialize CURL" << endl;
+        return 1;
+    }
+
+    string url = "https://api.coingecko.com/api/v3/coins/" + input;
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
+    curl_easy_setopt(curl, CURLOPT_CAINFO, "C:\\cacert.pem");
+
+   string response;
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+      cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
+        curl_easy_cleanup(curl);
+        return 1;
+    }
+
+    auto data = json::parse(response);
+    if (data.contains("market_data")) {
+        cout << "Price (USD): $" << data["market_data"]["current_price"]["usd"] << "\n";
+        cout << "24h Volume: $" << data["market_data"]["total_volume"]["usd"] << "\n";
+        cout << "24h Change: " << data["market_data"]["price_change_percentage_24h"] << "%\n";
+    } else {
+        cerr << "Could not find market data for this coin." << endl;
+    }
+
+    curl_easy_cleanup(curl);
+    return 0;
 }
-
 
 
     
